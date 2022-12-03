79d0551da25cae775ca2826655bf28a16e2fda8c
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-03 22:02:31 +0000

    con otras respuestas

--- codewars-taka/maxSquence.js
+++ codewars-taka/maxSquence.js
@@ -12,6 +12,42 @@ If the list is made up of only negative numbers, return 0 instead.
 Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 */
 
+/*
+  // el código de otros:
+
+  //1- itsPG, AlexanderDerGrosse, classic016, Fantom1991, user9878128, CHUENWEI, 14bce128@nirmauni.ac.in, dubdjon, AwsIdris, medokin (+ 125)
+  var maxSequence = function(arr){
+    var min = 0, ans = 0, i, sum = 0;
+    for (i = 0; i < arr.length; ++i) {
+      sum += arr[i];
+      min = Math.min(sum, min);
+      ans = Math.max(ans, sum - min);
+    }
+    return ans;
+  }
+
+  //2- hiasen, rscarlisle, Instigator, zhaihaoran, perception30, AhmedElhalaby, qwetexac, ccnklc, igof, PsychO_o, technomage (+ 17)
+  var maxSequence = function(arr){
+      var currentSum = 0;
+      return arr.reduce(function(maxSum, number){
+          currentSum = Math.max(currentSum+number, 0);
+          return Math.max(currentSum, maxSum);
+      }, 0);
+  }
+
+  //3- avaver, pitchblaknight, architechnium, medhichemlemsi
+  var maxSequence = function(arr){
+    var max = 0;
+    var cur = 0;
+    arr.forEach(function(i){
+      cur = Math.max(0, cur + i);
+      max = Math.max(max, cur);
+    });
+    return max;
+  }
+
+*/
+
 // La enviada
 var maxSequence = function (arr) {
 
