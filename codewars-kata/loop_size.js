377aa31449da1406cb6d5890b97b092ddd1c1ca7
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-04 18:35:31 +0000

    loop_size - soluciones

--- codewars-kata/loop_size.js
+++ codewars-kata/loop_size.js
@@ -23,9 +23,161 @@ Don't miss dmitry's article in the discussion after you pass the Kata !!
 ALGORITHMS, LINKED LISTS, PERFORMANCE
 */
 
-function loop_size(node){
+/*
+    No entiendo lo que hay que hacer ni cómo comprobarlo.
 
+De las soluciones (las más recientes)
+
+    //-1 ivanK_rep
+    // https://www.codewars.com/kata/reviews/52b020ce0b1d4565ec0003f2/groups/638ca977f4dbd8000197db29
+    function loop_size(node) {
+        let map = new Map();
+        let index = 0;
+        map.set(node, 0);
+
+        do {
+            index = index + 1;
+            node = node.getNext();
+
+            if (map.has(node)) {
+                return index - map.get(node);
+            }
+
+            map.set(node, index);
+        } while (node.next)
+
+        return 0;
+    }
+
+    //-2 douglas06mpp
+    // https://www.codewars.com/kata/reviews/52b020ce0b1d4565ec0003f2/groups/63884cc69a77550001d4df03
+    function loop_size(node) {
+        const map = new Map()
+        let count = 0
+        let currentNode = node
+
+        while (currentNode) {
+            count++
+            let nodeCount = map.get(currentNode)
+            if (nodeCount) {
+                return count - nodeCount
+            }
+
+            map.set(currentNode, count)
+            currentNode = currentNode.next
+        }
+
+        return 0
+    }
+
+    //1- laoris, c0deguy, dubdjon, rattLR, nicole_NT, Makemesuffer, nasnik, Strygevale, Protanton, Sergej-Karyuhin (+ 142)
+    // https://www.codewars.com/kata/reviews/52b020ce0b1d4565ec0003f2/groups/52b21ab778cdcd98af000c65
+    function loop_size(node) {
+        var turtle = node;
+        var rabbit = node;
+
+        // Find a point in the loop.  Any point will do!
+        // Since the rabbit moves faster than the turtle
+        // and the kata guarantees a loop, the rabbit will
+        // eventually catch up with the turtle.
+        do {
+            turtle = turtle.getNext();
+            rabbit = rabbit.getNext().getNext();
+        }
+        while (turtle != rabbit);
+
+        // The turtle and rabbit are now on the same node,
+        // but we know that node is in a loop.  So now we
+        // keep the turtle motionless and move the rabbit
+        // until it finds the turtle again, counting the
+        // nodes the rabbit visits in the mean time.
+        var count = 0;
+        do {
+            ++count;
+            rabbit = rabbit.getNext();
+        }
+        while (turtle != rabbit);
+
+        // voila
+        return count;
+    }
+
+*/
+
+//1- laoris, c0deguy, dubdjon, rattLR, nicole_NT, Makemesuffer, nasnik, Strygevale, Protanton, Sergej-Karyuhin (+ 142)
+// https://www.codewars.com/kata/reviews/52b020ce0b1d4565ec0003f2/groups/52b21ab778cdcd98af000c65
+function loop_size1(node) {
+    var turtle = node;
+    var rabbit = node;
+
+    // Find a point in the loop.  Any point will do!
+    // Since the rabbit moves faster than the turtle
+    // and the kata guarantees a loop, the rabbit will
+    // eventually catch up with the turtle.
+    do {
+        turtle = turtle.getNext();
+        rabbit = rabbit.getNext().getNext();
+    }
+    while (turtle != rabbit);
+
+    // The turtle and rabbit are now on the same node,
+    // but we know that node is in a loop.  So now we
+    // keep the turtle motionless and move the rabbit
+    // until it finds the turtle again, counting the
+    // nodes the rabbit visits in the mean time.
+    var count = 0;
+    do {
+        ++count;
+        rabbit = rabbit.getNext();
+    }
+    while (turtle != rabbit);
+
+    // voila
+    return count;
 }
+
+//-2 douglas06mpp
+// https://www.codewars.com/kata/reviews/52b020ce0b1d4565ec0003f2/groups/63884cc69a77550001d4df03
+function loop_size_2(node) {
+    const map = new Map()
+    let count = 0
+    let currentNode = node
+
+    while (currentNode) {
+        count++
+        let nodeCount = map.get(currentNode)
+        if (nodeCount) {
+            return count - nodeCount
+        }
+
+        map.set(currentNode, count)
+        currentNode = currentNode.next
+    }
+
+    return 0
+}
+
+//-1 ivanK_rep
+// https://www.codewars.com/kata/reviews/52b020ce0b1d4565ec0003f2/groups/638ca977f4dbd8000197db29
+function loop_size(node) {
+    let map = new Map();
+    let index = 0;
+    map.set(node, 0);
+
+    do {
+        index = index + 1;
+        node = node.getNext();
+
+        if (map.has(node)) {
+            return index - map.get(node);
+        }
+
+        map.set(node, index);
+    } while (node.next)
+
+    return 0;
+}
+
 
 /**
  * Indicar aquí la función a usar dentro de strictEqual
@@ -51,6 +203,8 @@ function loop_size(node){
          console.log("\tCorrecto!");
      }
  }
+
+//var Node = require('Node')
 
 // Pruebas
 let A = new Node(), B = new Node();
