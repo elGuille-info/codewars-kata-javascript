456c6fa1f143842e69cadfb42ac09d6debdf127c
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-04 13:53:30 +0000

    comp - funcionando

---
+++ codewars-taka/compSame.js
@@ -0,0 +1,105 @@
+﻿/*
+    # Are they the "same"?
+
+Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements,
+with the same multiplicities (the multiplicity of a member is the number of times it appears).
+"Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on.
+It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+Invalid arrays
+If, for example, we change the first number to something else, comp is not returning true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]
+b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+Remarks
+a or b might be [] or {} (all languages except R, Shell).
+a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+Note for C
+The two arrays have the same size (> 0) given as parameter in function comp.
+
+*/
+
+/**
+ * Indicar aquí la función a usar dentro de strictEqual
+ * @see strictEqual
+ */
+const laFuncion = comp;
+
+/**
+ * Para comprobar si el resultado de la función es válido.
+ *
+ * @param {*} valor El valor a comprobar.
+ * @param {*} resOK El resultado que debe dar.
+ * @see laFuncion Para asignar la función a usar.
+ */
+function strictEqual(valor1, valor2, resOK) {
+    console.log(valor1 + " =? " + valor2 + " = " + resOK);
+
+    let res = laFuncion(valor1, valor2);
+    if (res.toString() != resOK.toString()) {
+        console.log("\tNo es correcto. El resultado calculado es " + res + " debería ser " + resOK);
+    }
+    else {
+        console.log("\tCorrecto!");
+    }
+}
+
+
+function comp(array1, array2){
+    //your code here
+
+    // Si cualquiera de los arrays es nulo, devolver falso
+    if (array1 == null || array2 == null) {
+        return false;
+    }
+    // Si la longitud de los arrays es cero o no tienen el mismo tamaño, devolver falso
+    if ((array1.length + array2.length) == 0 || array1.length != array2.length) {
+        return false;
+    }
+
+    // buscar la potencia de 2 de array1 en array2
+    for (let n of array2) {
+        const powN = Math.sqrt(n);
+
+        if (array1.indexOf(powN) == -1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Pruebas
+strictEqual([], null, false);
+strictEqual([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361], true);
+strictEqual([121, 144, 19, 161, 19, 144, 19, 11], [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19], true);
+strictEqual([121, 144, 19, 161, 19, 144, 19, 11], [132, 14641, 20736, 361, 25921, 361, 20736, 361], false);
+strictEqual([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 36100, 25921, 361, 20736, 361], false);
+
+/*
+const { assert } = require('chai');
+
+describe("Tests", () => {
+  it("test", () => {
+    let a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+    let a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+    assert.isTrue(comp(a1, a2));
+  });
+});
+*/
