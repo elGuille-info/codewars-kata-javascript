2f61877b3e75f779c648b6677222fb7d5f5d92ba
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-06 20:48:24 +0000

    snail, falla en 1 random

--- codewars-kata/19-Snail Sot-snail.js
+++ codewars-kata/19-Snail Sot-snail.js
@@ -68,15 +68,15 @@ function snail(array) {
     }
     if (numSub > 3) {
         // 4a- Si hay más de 3 añadir los primeros desde el penúltimo al segundo array
-        for (let i = array[numSub - 1].length - 2; i > 0; i--) {
-            resultado.push(array[numSub - 1][0]);
+        for (let i = array[numSub - 2].length - 2; i > 0; i--) {
+            resultado.push(array[i][0]);
         }
         // 4b- Añadir del segundo desde el 2º al penúltimo
-        for (let i = 1; i < array[1].length - 2; i++) {
+        for (let i = 1; i <= array[1].length - 2; i++) {
             resultado.push(array[1][i]);
         }
         // 4c- añadir desde el 3º subarray al penúltimo, los penúltimos
-        for (let i = 3; i < numSub - 1; i++) {
+        for (let i = 2; i < numSub - 2; i++) {
             let j = array[i].length - 2;
             resultado.push(array[i][j]);
         }
@@ -88,49 +88,28 @@ function snail(array) {
         // Comprobar si son pares
         let pares = numSub % 2 == 0;
         if (pares) {
-            // del penúltimo-1 (numSub-3) al numSub-4, los que estén en la posición 1
-            for (let i = numSub-3; i <= numSub-4; i--) {
+            // del antepenúltimo (numSub-3) al numSub-4, los que estén en la posición 1
+            for (let i = numSub-3; i >= numSub-4; i--) {
                 resultado.push(array[i][1]);
             }
-            // del numSub-4, del 2º al antepenúltimo
-            for (let i = 1; i < array[numSub-4].length -3; i++) {
+            // del numSub-4, del 3º al antepenúltimo
+            for (let i = 2; i <= array[numSub-4].length -3; i++) {
                 resultado.push(array[numSub-4][i]);
             }
             // del numSub-3 del antepenúltimo al 3º (2)
-            for (let i = array[numSub-3].length -3; i < 3; i++) {
+            for (let i = array[numSub-3].length -3; i >= 2; i--) {
                 resultado.push(array[numSub-3][i]);
             }
         }
         else {
-            // 4e- Añadir los del final que estarán en el subarray del centro: 5/2 = 2.5 (2)
+            // 4e- Si son impares, añadir los del final que estarán en el subarray del centro: 5/2 = 2.5 (2)
             // del 2ª al antepenúltimo
-            let m = Math.ceil(numSub / 2);
-            for (let i = 1; i < array[m].length - 3; i++) {
+            let m = Math.floor(numSub / 2);
+            for (let i = 1; i <= array[m].length - 3; i++) {
                 resultado.push(array[m][i]);
             }
         }
     }
-
-    // // 5- Añadir los del final que estarán en el 2º empezando por el final
-    // let x = numSub - 2;
-    // for (let i = 0; i < array[x].length - 1; i++) {
-    //     resultado.push(array[x][i]);
-    // }
-
-    // // Ahora depende de los subarrays que haya
-    // // Si son 3: Añadir del array[1] del primero al penúltimo.
-    // if (numSub == 3) {
-    //     for (let i = 0; i < array[1].length - 1; i++) {
-    //         resultado.push(array[1][i]);
-    //     }
-    // }
-    // else {
-    //     // Si hay más de 3 sub arrays
-    //     let x = numSub - 2;
-    //     for (let i = 0; i < array[x].length - 1; i++) {
-    //         resultado.push(array[x][i]);
-    //     }
-    // }
 
     return resultado;
 }
@@ -156,26 +135,29 @@ function pruebas(arr, resOK) {
 // pruebas([[]], []);
 // pruebas([[1]], [1]);
 //pruebas([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]);
+
 // Con 5 subarrays
-pruebas([
-    [ 1,  2,  3,  4,  5],
-    [ 6,  7,  8,  9, 10],
-    [11, 12, 13, 14, 15],
-    [16, 17, 18, 19, 20],
-    [21, 22, 23, 24, 25]],
-    [
-        1,  2,  3,  4, 5,   // 1- Añadir al principio todos los elementos del primer array
-        10, 15, 20, 25,     // 2- Añadir el último de cada sub array (salvo la primera)
-        24, 23, 22, 21,     // 3- Del último array, añadir todos desde el penúltimo hasta el primero
-        16, 11, 6,          // 4a- Si hay más de 3 añadir los primeros desde el penúltimo al segundo
-        7, 8, 9,            // 4b- Añadir del segundo desde el 2º al penúltimo
-        14, 19,             // 4c- añadir desde el 3º al penúltimo, los penúltimos
-        18, 17,             // 4d- del penúltimo añadir desde el antepenúltimo al segundo
-        12, 13              // 4e- Si son impares Añadir los del final que estarán en el subarray del centro: 5/2 = 2.5 (2)
-                            // del 2ª al antepenúltimo
-    ]);
+// console.log("Con 5 subarrays");
+// pruebas([
+//     [ 1,  2,  3,  4,  5],
+//     [ 6,  7,  8,  9, 10],
+//     [11, 12, 13, 14, 15],
+//     [16, 17, 18, 19, 20],
+//     [21, 22, 23, 24, 25]],
+//     [
+//         1,  2,  3,  4, 5,   // 1- Añadir al principio todos los elementos del primer array
+//         10, 15, 20, 25,     // 2- Añadir el último de cada sub array (salvo la primera)
+//         24, 23, 22, 21,     // 3- Del último array, añadir todos desde el penúltimo hasta el primero
+//         16, 11, 6,          // 4a- Si hay más de 3 añadir los primeros desde el penúltimo al segundo
+//         7, 8, 9,            // 4b- Añadir del segundo desde el 2º al penúltimo
+//         14, 19,             // 4c- añadir desde el 3º al penúltimo, los penúltimos
+//         18, 17,             // 4d- del penúltimo añadir desde el antepenúltimo al segundo
+//         12, 13              // 4e- Si son impares añadir los del final que estarán en el subarray del centro: 5/2 = 2.5 (2)
+//                             // del 2ª al antepenúltimo
+//     ]);
 
 // Con 6 subarrays
+console.log("Con 6 subarrays");
 pruebas([
     [1, 2, 3, 4, 5, 6],
     [20, 21, 22, 23, 24, 7],
@@ -191,10 +173,22 @@ pruebas([
         21, 22, 23, 24,     // 4b- Añadir del segundo desde el 2º al penúltimo
         25, 26, 27,         // 4c- añadir desde el 3º al penúltimo, los penúltimos
         28, 29, 30,         // 4d- del penúltimo añadir desde el antepenúltimo al segundo
-        31, 32,             // si son pares (>3) del penúltimo-1 (numSub-3) al numSub-4, los que estén en la posición 1
-        33, 34,             // si son pares (>3) del numSub-4, del 2º al antepenúltimo
-        35, 36              // Si son pares (>3) del numSub-3 del antepenúltimo al 3º (2)
+        31, 32,             // si son pares del antepenúltimo (numSub-3) al numSub-4, los que estén en la posición 1
+        33, 34,             // si son pares del numSub-4, del 2º al antepenúltimo
+        35, 36              // Si son pares del numSub-3 del antepenúltimo al 3º (2)
     ]);
+
+/*
+[ 128,  207,  864,  333,  487,  475,  582,  531,  188,  882,  891,  387,  239,  958,  569,  332,  239,  207 ]
+*/
+pruebas([
+    [128, 207, 864, 333, 487, 475],
+    [582, 531, 188, 882, 891, 387],
+    [239, 958, 569, 332, 239, 207]
+],[
+    128, 207, 864, 333, 487, 475,
+    387, 207
+])
 //pruebas();
 
 /*
