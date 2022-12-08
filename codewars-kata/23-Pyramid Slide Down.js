c1c7c81fbd59e5cc82eedddef00a858d829cdeba
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-08 14:23:21 +0000

    longestSlideDown - comprendiendo el problema

--- codewars-kata/23-Pyramid Slide Down.js
+++ codewars-kata/23-Pyramid Slide Down.js
@@ -30,6 +30,17 @@ https://projecteuler.net/
 ALGORITHMS, DYNAMIC PROGRAMMING
 */
 
+/*
+  Creo que la idea es tomar el índice n de cada fila n, salvo las 2 primeras que es el índice 0
+
+[      3   ], // 0
+[      7, 4], // 0
+[2,    4, 6], // 1
+[8, 5, 9, 3]  // 2
+
+Aquí sería la suma de 3+7+4+9
+
+*/
 function longestSlideDown (pyramid) {
 
 }
@@ -59,6 +70,31 @@ function testArrNum(arr, resOK) {
        console.log("\tCorrecto!");
    }
 }
+
+// Pruebas
+testArrNum([[3],
+  [7, 4],
+  [2, 4, 6],
+  [8, 5, 9, 3]],
+  23);
+
+testArrNum( [[75],
+  [95, 64],
+  [17, 47, 82],
+  [18, 35, 87, 10],
+  [20,  4, 82, 47, 65],
+  [19,  1, 23, 75,  3, 34],
+  [88,  2, 77, 73,  7, 63, 67],
+  [99, 65,  4, 28,  6, 16, 70, 92],
+  [41, 41, 26, 56, 83, 40, 80, 70, 33],
+  [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+  [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+  [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+  [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+  [63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+  [ 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23]],
+  1074);
+
 
 /*
 const Test = require('@codewars/test-compat');
