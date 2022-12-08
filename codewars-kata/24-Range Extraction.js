e718b5a030924c598b9e23da8cf6be6c83dc0645
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-08 21:18:59 +0100

    Range Extraction - solution

---
+++ codewars-kata/24-Range Extraction.js
@@ -0,0 +1,68 @@
+/*
+    # Range Extraction (4 kyu)
+
+A format for expressing an ordered list of integers is to use a comma separated list of either individual integers
+or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'.
+The range includes all integers in the interval including both endpoints. 
+It is not considered a range unless it spans at least 3 numbers. For example "12,13,15-17"
+
+Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+
+Example:
+
+solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+// returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+Courtesy of rosettacode.org
+
+ALGORITHMS
+*/
+
+function solution(list){
+    // TODO: complete solution 
+}
+
+/**
+ * Indicar aquí la función a usar dentro de
+ * @see testArrNum
+ *
+ * Poner arriba el método usado para las pruebas, aunque no es necesario.
+ * Lo importante es asignar el valor a 'lafuncion', aunque eso se hace en el código a comprobar.
+ */
+let laFuncion = solution;
+
+/**
+ * Comprueba los cálculos a realizar sobre un array que devuelven un valor numérico.
+ *
+ * @param {*} arr El array a procesar.
+ * @param {*} resOK El valor correcto.
+ */
+function testArrNum(arr, resOK) {
+    var res = laFuncion(arr);
+    // Mostrar el array
+    //console.log("[" + arr.toString() + "] = " + resOK + " ?= (" + res + ")");
+    // No mostrar el array
+    console.log("Del array indicado la respuesta correcta es = " + resOK + " ?= (" + res + ")");
+    if (resOK != res) {
+        console.log("\tEl valor devuelto es " + res + " y debe ser " + resOK);
+    }
+    else {
+        console.log("\tCorrecto!");
+    }
+}
+
+// Pruebas
+testArrNum([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20], "-6,-3-1,3-5,7-11,14,15,17-20");
+
+
+
+/*
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Sample Tests", () => {
+  it("Should pass sample tests", () => {
+    assert.deepEqual(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]), "-6,-3-1,3-5,7-11,14,15,17-20")
+  });
+});
+*/
