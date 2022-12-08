65e1914c358fa984bcc242f52697c885e8970fba
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-08 13:36:04 +0000

    longestSlideDown - solución-explicación en la web

---
+++ otras pruebas/23-Pyramid Slide Down buscado.md
@@ -0,0 +1,11 @@
+﻿# Pyramid Slide Down
+
+[Codewars kata en JavaScript](https://www.codewars.com/kata/551f23362ff852e2ab000037/train/javascript)
+
+Un artículo sobre el tema:
+[[Write-Up] Pyramid Slide Down Solution Using JavaScript](https://handhikayp.medium.com/write-up-pyramid-slide-down-solution-using-javascript-80decd6d51de)
+
+La solución indicada en el artículo anterior:
+[railsstudent - longestSlideDown.js](https://gist.github.com/railsstudent/d40bd874d773794ff358c3465a6d2f5d)
+
+Ver el código en: railsstudent - longestSlideDown.js
---
+++ otras pruebas/23-railsstudent - longestSlideDown.js
@@ -0,0 +1,20 @@
+﻿
+// https://www.codewars.com/kata/pyramid-slide-down/train/javascript
+// longestSlideDown.js
+
+// http://stackoverflow.com/questions/8002252/euler-project-18-approach
+function longestSlideDown (pyramid) {
+  let pyramidSum = [];
+  pyramid.forEach((r, i) => {
+    pyramidSum.push(r.map((e) => {
+      return (i === pyramid.length - 1) ? e : 0;
+    }));
+  });
+
+  for (let i = pyramidSum.length - 2; i >= 0; i--) {
+      for (let j = 0; j < pyramidSum[i].length; j++) {
+          pyramidSum[i][j] = pyramid[i][j] + Math.max(pyramidSum[i+1][j], pyramidSum[i+1][j+1]);
+      }
+  }
+  return pyramidSum[0][0];
+}
