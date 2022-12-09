266ec8814c78acc410dd53c20df55e94fa9a0dcd
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-09 19:53:11 +0000

    parseInt - empezamos

---
+++ codewars-kata/25-parseInt reloaded.js
@@ -0,0 +1,68 @@
+﻿/*
+    # parseInt() reloaded (4 kyu)
+
+In this kata we want to convert a string into an integer. The strings simply represent the numbers in words.
+
+Examples:
+
+"one" => 1
+"twenty" => 20
+"two hundred forty-six" => 246
+"seven hundred eighty-three thousand nine hundred and nineteen" => 783919
+Additional Notes:
+
+The minimum number is "zero" (inclusively)
+The maximum number, which must be supported is 1 million (inclusively)
+The "and" in e.g. "one hundred and twenty-four" is optional, in some cases it's present and in others it's not
+All tested numbers are valid, you don't need to validate them
+
+*/
+
+function parseInt(string) {
+    // TODO: it's your task now
+}
+
+/**
+ * Indicar aquí la función a usar dentro de
+ * @see testArrNum
+ *
+ * Poner arriba el método usado para las pruebas, aunque no es necesario.
+ * Lo importante es asignar el valor a 'lafuncion', aunque eso se hace en el código a comprobar.
+ */
+let laFuncion = parseInt;
+
+/**
+ * Para comprobar si el resultado de la función es válido.
+ *
+ * @param {*} valor El número a evaluar por la función.
+ * @param {*} resOK El resultado que debe dar.
+ * @see laFuncion Para asignar la función a usar.
+ */
+function comparaResultado(valor, resOK) {
+    console.log(valor + " = " + resOK);
+
+    let res = laFuncion(valor);
+    if (res.toString() != resOK.toString()) {
+        console.log("\tNo es correcto. El resultado calculado es " + res + " debería ser " + resOK);
+    }
+    else {
+        console.log("\tCorrecto!");
+    }
+}
+
+// Pruebas
+comparaResultado('one', 1);
+comparaResultado('twenty', 20);
+comparaResultado('two hundred forty-six', 246);
+
+/*
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(parseInt('one'), 1);
+Test.assertEquals(parseInt('twenty'), 20);
+Test.assertEquals(parseInt('two hundred forty-six'), 246);
+  });
+});
+*/
