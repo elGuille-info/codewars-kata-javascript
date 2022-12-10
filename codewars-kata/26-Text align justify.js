0e938a111ff44092b2f764f4a4e70fc3fecb789c
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-10 16:16:19 +0000

    justify - con soluciones

--- codewars-kata/26-Text align justify.js
+++ codewars-kata/26-Text align justify.js
@@ -54,6 +54,140 @@ Also you can always take a look at how justification works in your text editor o
 Have fun :)
 */
 
+/*
+    Las soluciones
+*/
+
+//1- laoris, Gerald Chen, pleung75, homyak, MoodyBaboon, PierreRambaud, pompeu2004, przekot, kevlju, Lesha701 (+ 15)
+// https://www.codewars.com/kata/reviews/537e31ab8402c453a7000099/groups/5386686fee24bf1e950007b7
+
+/**
+ * @param {String} str - inital string
+ * @param {Number} len - line length
+ */
+function justify_1(str, len) {
+    var words = str.split(' ');
+    var lines = [];
+    var lastLine = words.reduce(function (line, word) {
+        if (line) {
+            if (line.length + word.length + 1 <= len)
+                return line + ' ' + word;
+            lines.push(line);
+        }
+        return word;
+    });
+    lines = lines.map(function (line) {
+        if (line.indexOf(' ') >= 0)
+            for (var lineLen = line.length; lineLen < len;)
+                line = line.replace(/ +/g, function (spaces) {
+                    return spaces + (lineLen++ < len ? ' ' : '');
+                });
+        return line;
+    });
+    lastLine && lines.push(lastLine);
+    return lines.join('\n');
+}
+
+//2- davazp, vasilukwolf, BrianJenney, naidu@123, asemle, AyushBk, johntoppo, Mac Santos, Vlinder, user7820265 (+ 7)
+// https://www.codewars.com/kata/reviews/537e31ab8402c453a7000099/groups/5449970eec342c36f200117d
+
+/**
+ * @param {String} str - inital string
+ * @param {Number} len - line length
+ */
+var justify_2 = function (str, len) {
+    var words = str.split(' ');
+    var output = [];
+    while (words.length) {
+        // Collect as many words as possible for the current line
+        var lineWords = [];
+        while (words.length > 0 && (lineWords + ',' + words[0]).length <= len)
+            lineWords.push(words.shift());
+
+        if (words.length) {  // No last line, so justify it
+            // Compute the holes between the words
+            var spaces = len - ('' + lineWords).length;
+            for (var i = 0; i < spaces; i++)
+                lineWords[i % (lineWords.length - 1)] += ' ';
+        }
+
+        // Build the line
+        output.push(lineWords.join(' '));
+    }
+
+    return output.join('\n');
+};
+
+//3- chikh
+// https://www.codewars.com/kata/reviews/537e31ab8402c453a7000099/groups/55bf6b208ab820a0f1000032
+var collectLines = function (len) {
+    return function (lines, word) {
+        var lastLine = lines[lines.length - 1];
+        var lastLineWithNextWord = lastLine.slice(0);
+        lastLineWithNextWord.push(word);
+        if (lastLineWithNextWord.join(' ').length <= len) {
+            lastLine.push(word);
+        } else {
+            lines.push([word]);
+        }
+        return lines;
+    }
+};
+
+var addSpaces = function (len) {
+    return function (line, lineIdx, lines) {
+        if (lineIdx === lines.length - 1) {
+            return line;
+        }
+        var howManySpacesToAdd = len - line.join(' ').length;
+        var howManyToEveryWord = Math.floor(howManySpacesToAdd / (line.length - 1));
+        var howManyWordToIncreaseByOne = howManySpacesToAdd % (line.length - 1);
+        return line.map(function (word, wordNo) {
+            if (wordNo < line.length - 1)
+                return word + Array(howManyToEveryWord + 1).join(' ') + ((wordNo < howManyWordToIncreaseByOne) ? ' ' : '');
+            else {
+                return word;
+            }
+        });
+    }
+};
+
+/**
+ * @param {String} str - inital string
+ * @param {Number} len - line length
+ */
+var justify_3 = function (str, len) {
+    return str.
+        split('\ ').
+        reduce(collectLines(len), [[]]).
+        map(addSpaces(len)).
+        map(function (line) {
+            return line.join(' ')
+        }).join('\n');// Your code goes here
+};
+
+//4- zero-byte
+// https://www.codewars.com/kata/reviews/537e31ab8402c453a7000099/groups/566927257a40ef8def000046
+var justify_4 = function (str, len) {
+    return str.split(" ").reduce((r, w, i, arr) => {
+        let line = r.pop() || [];
+        let lineLen = line.reduce((k, v) => k + v.length, Math.max(0, line.length));
+        if (lineLen + w.length > len) {
+            let index = 0;
+            while (lineLen++ <= len && line.length > 1) {
+                line[index] += " ";
+                index = (++index) % (line.length - 1);
+            }
+            r.push(line.join(" "), [w]);
+        } else if (i === arr.length - 1) {
+            r.push(line.concat(w).join(" "));
+        } else {
+            r.push(line.concat(w));
+        }
+        return r;
+    }, []).join("\n");
+};
+
 function justify0(text, width) {
     // Your code goes here
     return text
