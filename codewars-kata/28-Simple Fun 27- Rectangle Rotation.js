84decf36757a719e141baf323c891a85e80b1306
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-11 17:16:46 +0100

    rectangleRotation - con soluciones

--- codewars-kata/28-Simple Fun 27- Rectangle Rotation.js
+++ codewars-kata/28-Simple Fun 27- Rectangle Rotation.js
@@ -36,6 +36,109 @@ PUZZLES
 */
 
 /*
+    Soluciones
+*/
+
+//1- xantix, karimov_ads
+// https://www.codewars.com/kata/reviews/5886e118206a6530bf00079a/groups/5e5e9d3d1cff4900017347df
+function rectangleRotation_1(a, b) {
+
+    /* If we mark the inner points based on (E)ven and (O)dd 
+       Manhatten distance from the origin. We end up with 
+       an odd and an even rectangle. For example with (6,4):
+       
+      4 3 2 1 0 1 2 3 4
+      
+      3         E
+               / \
+      2       E O E
+             / / \ \
+      1     E O E O E  
+           / /   / /
+      0   E O E O E
+         / /   / /
+      1 E O E O E
+         \ \ / /
+      2   E O E
+           \ / 
+      3     E
+      
+      4
+    
+      To get the whole number of points, we just need to:
+      1. Find the dimension of the even rectangle (5 by 3)
+      2. Find the dimensions of the odd rectangle (4 by 2)
+      3. Multiply and add together (5*3) + (4*2) = 23
+      
+      To find the dimensions of the even rectangle we can count 
+      the number of hops of length sqrt(2) that it takes to start
+      at the origin and move North-East until we reach the edge 
+      of the rectangle (half of a). Multiply by two, but subtract 
+      one so we don't double count the origin. Repeat North-West.
+      
+      To find the dimensions of the odd rectangle we can start
+      by walking one grid point East if we can. (we can do this
+      since a and b are both >= 2). Doing so, takes us just a 
+      little bit closer to the top right side of the 
+      rectangle (by 1/sqrt(2)). From this point see how many
+      North-East hops of length sqrt(2) it takes to reach the edge 
+      of the rectangle (half of a). Mutliply by two. 
+      Repeat South-East.
+    */
+
+    const ceil = Math.ceil;
+    const sqrt2 = Math.sqrt(2);
+    const invSqrt2 = 1 / sqrt2;
+    const halfA = a / 2;
+    const halfB = b / 2;
+
+    const evenDimension1 = (ceil(halfA / sqrt2) * 2) - 1;
+    const evenDimension2 = (ceil(halfB / sqrt2) * 2) - 1;
+
+    const evenPoints = evenDimension1 * evenDimension2;
+
+    const oddDimension1 = ceil((halfA - invSqrt2) / sqrt2) * 2;
+    const oddDimension2 = ceil((halfB - invSqrt2) / sqrt2) * 2;
+
+    const oddPoints = oddDimension1 * oddDimension2;
+
+    return oddPoints + evenPoints;
+}
+
+//2- cave.on, alvar91, user2451771, 任自博, mon3emm, Denis_Narol
+// https://www.codewars.com/kata/reviews/5886e118206a6530bf00079a/groups/5932d7f754491dc811000613
+function rectangleRotation_2(a, b) {
+
+    let h = a / Math.sqrt(2) | 0
+        , v = b / Math.sqrt(2) | 0;
+
+    return h * v + (h + 1) * (v + 1) - (h % 2 ^ v % 2);
+
+}
+
+//3- 0lexa
+// https://www.codewars.com/kata/reviews/5886e118206a6530bf00079a/groups/5f5bef2869f1cd0001e11aeb
+const rectangleRotation_3 = (a, b) =>
+    (fn => (fn(a) * fn(b) + (fn(a) + fn(b)) / 2 ^ 0) * 2 + 1)
+        (val => val / Math.SQRT2 ^ 0);
+
+
+//4- Olegeant
+// https://www.codewars.com/kata/reviews/5886e118206a6530bf00079a/groups/6265ee106a8fbd00017db016
+const rectangleRotation_4 = (a, b) =>
+    (F = (side, d) => ~~(side / 2 / 2 ** .5 + .5 + d) * 2 - d * 2)(a, 0) * F(b, 0) + F(a, .5) * F(b, .5);
+
+
+// 5 - dfhwze, Sayari-Amir, fadedreams
+// https://www.codewars.com/kata/reviews/5886e118206a6530bf00079a/groups/60b68803312b5300016f5cb3
+function rectangleRotation_5(a, b) {
+    let x = Math.floor(a / Math.sqrt(2));
+    let y = Math.floor(b / Math.sqrt(2));
+    let r = (x + 1) * (y + 1) + x * y;
+    return r + r % 2 - 1;
+}
+
+/*
     La solución de gtfo (https://velog.io/@gtfo/Simple-Fun-27-Rectangle-Rotation)
 */
 function rectangleRotation_gtfo(a, b) {
@@ -72,73 +175,26 @@ def rectangle_rotation(a, b):
         sum = sum - 1
     return sum    
 */
-
-// La presentada
-function rectangleRotation(a, b) {
-    let a1 = Math.floor(a / Math.sqrt(2)) + 1;
-    let b1 = Math.floor(b / Math.sqrt(2)) + 1;
-    let res = a1 * b1 + (b1 - 1) * (a1 - 1);
-    if (res % 2 == 0) 
-        res--;
-    return res;
-}
-
 function rectangleRotation1(a, b) {
     let a2 = a / Math.sqrt(2);
     let b2 = b / Math.sqrt(2);
     let a3 = Math.floor(a2) + 1;
     let b3 = Math.floor(b2) + 1;
     let res = a3 * b3 + (b3 - 1) * (a3 - 1);
-    if (res % 2 == 0) 
+    if (res % 2 == 0)
         res--;
     return res;
 }
 
-function rectangleRotation0(a, b) {
-    //coding and coding..
-    //console.log("a: " + a + " b: "+b)
-
-    //let res = (b-1) * (a-1) + (b-2) * (a-2);
-    let res = (a-1) * (b-1) + (a-2) * (b-2);
-
+// La presentada
+function rectangleRotation(a, b) {
+    let a1 = Math.floor(a / Math.sqrt(2)) + 1;
+    let b1 = Math.floor(b / Math.sqrt(2)) + 1;
+    let res = a1 * b1 + (b1 - 1) * (a1 - 1);
+    if (res % 2 == 0)
+        res--;
     return res;
-
 }
-
-function rectanglePoints(a, b) {
-    const c = Math.ceil(Math.sqrt(Math.pow(a / 2, 2) + Math.pow(b / 2, 2)));
-    let x1 = (a / 2) * Math.sqrt(2);
-    let y1 = (b / 2) * Math.sqrt(2);
-    let x2 = -x1;
-    let y2 = -y1;
-
-    let area = a * b;
-    //console.log("\tarea = " + area);
-    let sideLength = 0;
-    // for (let x = Math.ceil(x1); x >= x2; x--) {
-    //     for (let y = Math.ceil(y1); y >= y2; y--) {
-    //         // Check if the point is on the sides of the rectangle.
-    //         if (x * x + y * y == (x1 * x1 + y1 * y1)) {
-    //             // Check if the point is not at a corner of the rectangle.
-    //             if (x != x1 && x != x2 && y != y1 && y != y2) {
-    //                 sideLength++;
-    //             }
-    //         }
-    //     }
-    // }
-    //sideLength = 2 * (a + b) - 4;
-    //sideLength = 2 * (a + b) - 8;
-    //sideLength = (2 * (a + b) - 8) / 2;
-    //sideLength = (2 * (a + b) - 8 + 4) / 2;
-    //sideLength = (2 * (a + b) - 8 + 4 / 2) / 2;
-    //sideLength = (2 * (a + b) - 8 + (4 / 2)) / 2;
-    //sideLength   = (2.0 * (a + b) - 8) / 2.0 + 1;
-    sideLength = 2 * c - 4;
-    let res = (a-1) * (b-1) + (a-2) * (b-2);
-    sideLength = res;
-    return area * c - sideLength;
-}
-
 
 /**
  * Indicar aquí la función a usar dentro de
