41366a047a9fe021429625e63c56db32268dd429
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-11 20:29:50 +0100

    userRank - las soluciones

--- codewars-kata/29-(no)-Codewars style ranking system.js
+++ codewars-kata/29-(no)-Codewars style ranking system.js
@@ -46,6 +46,89 @@ It uses a pure Math based solution that gives consistent results no matter what 
 ALGORITHMS
 */
 
+/*
+  Soluciones
+
+
+//1- adriaanbaelus, kieutridang, user3469798, Gowthami0301, ulyxgwdimo, Karimof_urullo, codewars11111
+// https://www.codewars.com/kata/reviews/521679fd64dc2cdac0000140/groups/5250c16e1777d16198000409
+var User = (function () {
+  ////////// Hidden //////////
+  var hierarchy = [ -8,-7,-6,-5,-4,-3,-2,-1, 1, 2, 3, 4, 5, 6, 7, 8 ];
+  var progress = { min: 0, max: 100 };
+  var rank = { min: hierarchy[0], max: hierarchy[hierarchy.length - 1] };
+  
+  progress.acceleration = function (userRank, activityRank) {
+    var d = rank.difference(userRank, activityRank);
+    if (d === -1)       return 1;
+    else if (d === 0)   return 3;
+    else if (d > 0)     return 10 * d * d;
+    else                return 0;
+  };
+  progress.update = function (user, acceleration) {
+    user.progress += acceleration;
+    user.progress = (user.rank === rank.max) ? progress.min : user.progress % progress.max;
+  };
+  rank.difference = function (userRank, activityRank) {
+    return hierarchy.indexOf(activityRank) - hierarchy.indexOf(userRank);
+  };
+  rank.update = function (user, acceleration) {
+    var d = ~~((user.progress + acceleration) / progress.max);
+    var i = hierarchy.indexOf(user.rank) + d;
+    if (i >= hierarchy.length) i = hierarchy.length -1;
+    user.rank = hierarchy[i];
+  };
+  rank.valid = function (r) {
+    return hierarchy.indexOf(r) > -1;
+  };
+  
+  ////////// Exposed //////////
+  var User = function () {
+    this.progress = progress.min;
+    this.rank = rank.min;
+  };
+  User.prototype.incProgress = function (activityRank) {
+    if (!rank.valid(activityRank)) throw new Error("Invalid activity rank given");
+    var accel = progress.acceleration(this.rank, activityRank);
+    rank.update(this, accel);
+    progress.update(this, accel);
+  };
+  return User;
+}).call();
+
+
+//2- systimotic, user6538049, user3278724
+// https://www.codewars.com/kata/reviews/521679fd64dc2cdac0000140/groups/5ab39e76ca1f6b923d00006b
+class User {
+  constructor() {
+    this.totalProgress = 0;
+  }
+  
+  get progress() {
+    return this.rank < 8 ? this.totalProgress % 100 : 0;
+  }
+  
+  get rank() {
+    let rank = Math.floor(this.totalProgress / 100) - 8;
+    return rank >= 0 ? rank + 1 : rank;
+  }
+  
+  incProgress(rank) {
+    if (rank === 0 || rank < -8 || rank > 8) {
+      throw new Error('Rank out of bounds');
+    }
+    
+    if (rank > 0  && this.rank < 0) {
+      rank--;
+    }
+
+    const diff = rank - this.rank;
+    this.totalProgress += diff > 0 ? (10 * diff * diff) : diff < 0 ? 1 : 3;
+  }
+}
+
+*/
+
 // TODO: create the User class/object
 // it must support rank, progress and the incProgress(rank) method
 
