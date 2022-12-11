e8c0f6fd249fe158ce8cd5d82bb21eb4829d1111
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-11 20:55:08 +0100

    formatDuration - las soluciones

--- codewars-kata/30-Human readable duration format.js
+++ codewars-kata/30-Human readable duration format.js
@@ -31,6 +31,89 @@ A unit of time must be used "as much as possible". It means that the function sh
 STRINGS, DATE TIME, ALGORITHMS
 */
 
+/*
+    Las soluciones
+*/
+
+//1- omgo, suva, summeranimation, dubdjon, codenao, user9127485, Pablo Sabater, qimingzizhennan, Gperezg, testfy (+ 1158)
+// https://www.codewars.com/kata/reviews/52742f58faf5485cae000b9d/groups/536b6acd1da4dcb0ac00090a
+function formatDuration_1(seconds) {
+    var time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 },
+        res = [];
+
+    if (seconds === 0) return 'now';
+
+    for (var key in time) {
+        if (seconds >= time[key]) {
+            var val = Math.floor(seconds / time[key]);
+            res.push(val += val > 1 ? ' ' + key + 's' : ' ' + key);
+            seconds = seconds % time[key];
+        }
+    }
+
+    return res.length > 1 ? res.join(', ').replace(/,([^,]*)$/, ' and' + '$1') : res[0]
+}
+
+//2- Krapock, Mary Good, user6723683, khairul, hobiafk@gmail.com, eQuality, Vladislav-Fedurkin, user13131313, Heinrich-Marx, Takahashi (+ 19)
+// https://www.codewars.com/kata/reviews/52742f58faf5485cae000b9d/groups/542ab4c1813f1ddb07000a56
+function formatDuration_2(seconds) {
+    if (!seconds) return "now";
+    var strout = "";
+    var s = seconds % 60;
+    seconds = (seconds - s) / 60;
+    var m = seconds % 60;
+    seconds = (seconds - m) / 60;
+    var h = seconds % 24;
+    seconds = (seconds - h) / 24;
+    var d = seconds % 365;
+    seconds = (seconds - d) / 365;
+    var y = seconds;
+
+    var english = [];
+    if (y) english.push(y + " year" + (y > 1 ? 's' : ''));
+    if (d) english.push(d + " day" + (d > 1 ? 's' : ''));
+    if (h) english.push(h + " hour" + (h > 1 ? 's' : ''));
+    if (m) english.push(m + " minute" + (m > 1 ? 's' : ''));
+    if (s) english.push(s + " second" + (s > 1 ? 's' : ''));
+
+    return english.join(", ").replace(/,([^,]*)$/, " and$1");
+
+}
+
+//3- allonhadaya, rsschool_17abaff0b80ff8f3, Dr.Sweet
+// https://www.codewars.com/kata/reviews/52742f58faf5485cae000b9d/groups/5539328c8601e7628b0000a7
+var formatDuration_3 = (function () {
+
+    return function formatDuration(seconds) {
+        return [{ name: 'year', size: 365 * 24 * 60 * 60 * 1 },
+        { name: 'day', size: 24 * 60 * 60 * 1 },
+        { name: 'hour', size: 60 * 60 * 1 },
+        { name: 'minute', size: 60 * 1 },
+        { name: 'second', size: 1 }].
+            reduce(parse, { parts: [], seconds: seconds }).
+            parts.
+            reduce(join, 'now');
+    };
+
+    function parse(result, part) {
+        var quantity = Math.floor(result.seconds / part.size);
+        if (quantity > 0) {
+            result.seconds -= quantity * part.size;
+            result.parts.push(quantity + ' ' + part.name + (quantity == 1 ? '' : 's'));
+        }
+        return result;
+    }
+
+    function join(result, part, index, arr) {
+        switch (index) {
+            case 0: return part;
+            case arr.length - 1: return result + ' and ' + part;
+            default: return result + ', ' + part;
+        }
+    }
+
+}());
+
 function formatDuration0(seconds) {
     // Complete this function
 }
