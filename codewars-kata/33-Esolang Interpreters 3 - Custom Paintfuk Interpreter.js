4fce5da5b30eeb19cabf6fed2c24de6756e0c948
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-12 20:50:28 +0100

    interpreter - preparando

--- codewars-kata/33-Esolang Interpreters 3 - Custom Paintfuk Interpreter.js
+++ codewars-kata/33-Esolang Interpreters 3 - Custom Paintfuk Interpreter.js
@@ -65,6 +65,74 @@ function interpreter(code, iterations, width, height) {
     // Implement your interpreter here
 }
 
+
+/**
+ * Indicar aquí la función a usar dentro de
+ * @see comparaResultado
+ *
+ * Poner arriba el método usado para las pruebas, aunque no es necesario.
+ * Lo importante es asignar el valor a 'lafuncion', aunque eso se hace en el código a comprobar.
+ */
+let laFuncion = interpreter;
+
+/**
+ * La función a usar recibe los 4 primeros parámetros.
+ * 
+ * @param {*} code 
+ * @param {*} iterations 
+ * @param {*} width 
+ * @param {*} height 
+ * @param {*} resOK El resultado que debe producir.
+ */
+function comparaResultado(code, iterations, width, height, resOK) {
+    //console.log(valor + " = " + resOK);
+
+    //let res = laFuncion(valor);
+    let res = displayActual(laFuncion(code, iterations, width, height));
+    let res2 = displayExpected(resOK);
+
+    if (res != res2) {
+        console.log("\tNo es correcto. El resultado calculado es '" + res + "'");
+    }
+    else {
+        console.log("\tCorrecto!");
+    }
+}
+
+
+// Prints representation of datagrid - 0's are black and 1's are white
+// Note: prettyPrint() only works properly if your interpreter returns a representation of the datagrid in the correct format
+function prettyPrint(datagrid) {
+    var consoleOutput = "<pre>", copy = datagrid.split("\r\n");
+    for (let i = 0; i < copy.length; i++) {
+        for (let j = 0; j < copy[i].length; j++) {
+            consoleOutput += `<span style="color:${copy[i][j] === "0" ? "black" : "white"};background-color:${copy[i][j] === "0" ? "black" : "white"}">xx</span>`;
+        }
+        consoleOutput += "<br />";
+    }
+    consoleOutput += "</pre>";
+    console.log(consoleOutput);
+    return datagrid;
+}
+// Displays the grid your interpreter returns
+function displayActual(actual) {
+    console.log("You returned:");
+    return prettyPrint(actual);
+}
+// Displays the expected final state of datagrid
+function displayExpected(expected) {
+    console.log("Expected final state of data grid:");
+    return prettyPrint(expected);
+}
+
+// Pruebas
+
+Test.assertEquals(displayActual(interpreter("*e*e*e*es*es*ws*ws*w*w*w*n*n*n*ssss*s*s*s*", 0, 6, 9)), displayExpected("000000\r\n000000\r\n000000\r\n000000\r\n000000\r\n000000\r\n000000\r\n000000\r\n000000"), "Your interpreter should initialize all cells in the datagrid to 0");
+Test.assertEquals(displayActual(interpreter("*e*e*e*es*es*ws*ws*w*w*w*n*n*n*ssss*s*s*s*", 7, 6, 9)), displayExpected("111100\r\n000000\r\n000000\r\n000000\r\n000000\r\n000000\r\n000000\r\n000000\r\n000000"), "Your interpreter should adhere to the number of iterations specified");
+Test.assertEquals(displayActual(interpreter("*e*e*e*es*es*ws*ws*w*w*w*n*n*n*ssss*s*s*s*", 19, 6, 9)), displayExpected("111100\r\n000010\r\n000001\r\n000010\r\n000100\r\n000000\r\n000000\r\n000000\r\n000000"), "Your interpreter should traverse the 2D datagrid correctly");
+Test.assertEquals(displayActual(interpreter("*e*e*e*es*es*ws*ws*w*w*w*n*n*n*ssss*s*s*s*", 42, 6, 9)), displayExpected("111100\r\n100010\r\n100001\r\n100010\r\n111100\r\n100000\r\n100000\r\n100000\r\n100000"), "Your interpreter should traverse the 2D datagrid correctly for all of the \"n\", \"e\", \"s\" and \"w\" commands");
+Test.assertEquals(displayActual(interpreter("*e*e*e*es*es*ws*ws*w*w*w*n*n*n*ssss*s*s*s*", 100, 6, 9)), displayExpected("111100\r\n100010\r\n100001\r\n100010\r\n111100\r\n100000\r\n100000\r\n100000\r\n100000"), "Your interpreter should terminate normally and return a representation of the final state of the 2D datagrid when all commands have been considered from left to right even if the number of iterations specified have not been fully performed");
+
 /*
 const Test = require('@codewars/test-compat');
 
