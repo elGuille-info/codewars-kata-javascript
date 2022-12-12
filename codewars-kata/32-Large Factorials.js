5f60b2eda7c033231c31a57c6ea1e822c91caf68
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-12 20:08:45 +0100

    factorial - parece que va bien

--- codewars-kata/32-Large Factorials.js
+++ codewars-kata/32-Large Factorials.js
@@ -22,28 +22,58 @@ I have removed the use of require in the javascript language.
 ALGORITHMS, BIG INTEGERS
 */
 
-function factorial(n) {
-    // Add some code
-    let res = fact(n);
-    return res.toString();
-}
+/*
+// De: https://gist.github.com/e-mihaylin/9f54f545bff70e6f8244f4e07dd35310
+const factorial = n => {
+    if (!n) return '1'
+    let i, next, carret;
+    const result = n.toString().split``.reverse().map(Number);
 
-function fact(n) {
-    if (n > 18) {
-        return factorialStr(n);
+    while (--n) {
+        i = carret = 0;
+        while ((next = result[i++]) !== undefined || carret) {
+            carret += n * (next || 0);
+            result[i - 1] = carret % 10;
+            carret = parseInt(carret / 10);
+        }
     }
-    return (n != 1) ? n * fact(n - 1) : 1;
+
+    return result.reverse().join``;
+}
+//https://www.codewars.com/kata/large-factorials/javascript
+*/
+
+function factorial_mihaylin(n) {
+    if (!n) return '1'
+    let i, next, carret;
+    const result = n.toString().split``.reverse().map(Number);
+
+    while (--n) {
+        i = carret = 0;
+        while ((next = result[i++]) !== undefined || carret) {
+            carret += n * (next || 0);
+            result[i - 1] = carret % 10;
+            carret = parseInt(carret / 10);
+        }
+    }
+
+    return result.reverse().join``;
 }
 
-function factorialStr(n) {
-    return multiplyStrings(n, factorialStr(n - 1));
-}
+function factorial_for(number) {
+    if (number == 0) return '1';
+    if (number == 2) return '2';
 
-function factorial0(n) {
-    // Add some code
+    let res = number.toString();
+    // Calcularlo multiplicando el número hasta el 2.
+    for (let i = number - 1; i > 1; i--)
+    {
+        //number *= i;
+        res = multiplyStrings(res, i.toString());
+    }
+    //return number;
+    return res;
 
-    //return (n != 1) ? n * factorial(n - 1) : 1;
-    return multiplyStrings(n, factorial(n - 1));
 }
 
 function multiplyStrings(a, b) {
@@ -77,6 +107,30 @@ function multiplyStrings(a, b) {
     return result;
 }
 
+function factorial(n) {
+    // Add some code
+    let res = fact(n);
+    return res.toString();
+}
+
+function fact(n) {
+    if (n > 18) {
+        return factorialStr(n);
+    }
+    return (n != 1) ? n * fact(n - 1) : 1;
+}
+
+function factorialStr(n) {
+    return multiplyStrings(n, factorialStr(n - 1));
+}
+
+function factorial0(n) {
+    // Add some code
+
+    //return (n != 1) ? n * factorial(n - 1) : 1;
+    return multiplyStrings(n, factorial(n - 1));
+}
+
 /**
  * Indicar aquí la función a usar dentro de
  * @see testArrNum
@@ -84,7 +138,7 @@ function multiplyStrings(a, b) {
  * Poner arriba el método usado para las pruebas, aunque no es necesario.
  * Lo importante es asignar el valor a 'lafuncion', aunque eso se hace en el código a comprobar.
  */
-let laFuncion = factorial;
+let laFuncion = factorial_for;
 
 /**
  * Para comprobar si el resultado de la función es válido.
