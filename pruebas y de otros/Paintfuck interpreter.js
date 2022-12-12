a041fd754fba215d44883279e37667016a13e4ab
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-12 22:28:01 +0100

    Paintfuck interpreters

---
+++ pruebas y de otros/Paintfuck interpreter.js
@@ -0,0 +1,82 @@
+// Paintfuck interpreter.js
+// Intento de adaptar Paintfuck interpreter.py
+/*
+# Paintfuck interpreter.py
+# https://github.com/lichkingwulaa/Codewars/blob/master/4_kyu_Esolang_Interpreters_3_Custom_Paintfuck_Interpreter.py
+
+
+def interpreter(code, iterations, width, height):
+    code = "".join(c for c in code if c in "[news]*")
+    canvas = [[0] * width for _ in range(height)]
+    row = col = step = count = loop = 0
+	
+    while step < len(code) and count < iterations:
+        command = code[step]
+    	
+        if loop:
+            if command == "[":
+                loop += 1
+            elif command == "]":
+                loop -= 1
+    	
+        elif command == "n":
+            row = (row - 1) % height
+        elif command == "s":
+            row = (row + 1) % height
+        elif command == "w":
+            col = (col - 1) % width
+        elif command == "e":
+            col = (col + 1) % width
+        elif command == "*":
+            canvas[row][col] ^= 1
+        elif command == "[" and canvas[row][col] == 0:
+            loop += 1
+        elif command == "]" and canvas[row][col] != 0:
+            loop -= 1
+    	
+        step += 1 if not loop else loop // abs(loop)
+        count += 1 if not loop else 0
+	
+    return "\r\n".join("".join(map(str, row)) for row in canvas)
+
+*/
+
+function interpreter(code, iterations, width, height) {
+    code = code.replaceAll("[^nesw\\[\\]*]*", "");
+    //code = "".join(c for c in code if c in "[news]*")
+    let canvas =[[height][width]] //[[0] * width for _ in range(height)]
+    //row = col = step = count = loop = 0
+    let row = col = step = count = loop = 0;
+
+    while (step < len(code) && count < iterations) {
+        command = code[step]
+
+        if (loop) {
+            if (command == "[") {
+                loop += 1
+            } else if (command == "]") {
+                loop -= 1
+            }
+        } else if (command == "n") {
+            row = (row - 1) % height
+        } else if (command == "s") {
+            row = (row + 1) % height
+        } else if (command == "w") {
+            col = (col - 1) % width
+        } else if (command == "e") {
+            col = (col + 1) % width
+        } else if (command == "*") {
+            canvas[row][col] ^= 1
+        } else if (command == "[" && canvas[row][col] == 0) {
+            loop += 1
+        } else if (command == "]" && canvas[row][col] != 0) {
+            loop -= 1
+        }
+
+        //step += 1 if not loop else loop // abs(loop)
+        //count += 1 if not loop else 0
+    }
+
+    //return "\r\n".join("".join(map(str, row)) for row in canvas)
+
+}
