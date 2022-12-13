73fc64d1082371dc5e68609909174e78c01a110c
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-13 20:25:22 +0100

    square - soluciones

--- codewars-kata-8 kyu/04-Function 2 - squaring an argument.js
+++ codewars-kata-8 kyu/04-Function 2 - squaring an argument.js
@@ -6,11 +6,25 @@ Now you have to write a function that takes an argument and returns the square o
 FUNDAMENTALS
 */
 
+/*
+    Soluciones
+*/
+
+//1- y 2-
+// https://www.codewars.com/kata/reviews/523b623152af8a30c600002a/groups/560d966bae9b412c50000098
+const square_1 = (n) => n * n;
+
+//3-
+// https://www.codewars.com/kata/reviews/523b623152af8a30c600002a/groups/56d804e911262de92e000b64
+function square_3(x) {
+    return x ** 2;
+}
+
 // Write the "square"-function here
 
 // La presentada
 function square(n) {
-    return n*n;
+    return n * n;
 }
 
 
