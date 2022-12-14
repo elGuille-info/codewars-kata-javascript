eed470f2995c291f7e3ef0ac4930a1d6e30426aa
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-14 19:59:04 +0100

    toAlternatingCase - soluciones

--- codewars-kata-8 y 7 kyu/8-kyu-15-altERnaTIng cAsE.js
+++ codewars-kata-8 y 7 kyu/8-kyu-15-altERnaTIng cAsE.js
@@ -20,10 +20,83 @@ As usual, your function/method should be pure, i.e. it should not mutate the ori
 FUNDAMENTALS
 */
 
-String.prototype.toAlternatingCase = function () {
-    // Define your method here :)
+/*
+    Soluciones
+*/
+
+//1-
+// https://www.codewars.com/kata/reviews/56efe986900453758d00008e/groups/56f3d7802010832bb9000bb1
+String.prototype.toAlternatingCase_1 = function () {
+    return this.split("").map(a => a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()).join('')
 }
 
+//2- 
+// https://www.codewars.com/kata/reviews/56efe986900453758d00008e/groups/57a02a3853ba336f6e000349
+String.prototype.toAlternatingCase_2 = function () {
+    new_str = "";
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] === this[i].toUpperCase()) {
+            new_str += this[i].toLowerCase();
+        }
+        else {
+            new_str += this[i].toUpperCase();
+        }
+    }
+    return new_str;
+}
+
+//3- 
+// https://www.codewars.com/kata/reviews/56efe986900453758d00008e/groups/56f1e9e7a138a0436d001857
+const isLowerCase = (char) => char.toLowerCase() === char;
+const swapCase = (char) => isLowerCase(char) ? char.toUpperCase() : char.toLowerCase();
+
+String.prototype.toAlternatingCase_3 = function () {
+    return [...this].map(swapCase).join('');
+};
+
+// La presentada
+String.prototype.toAlternatingCase = function () {
+    // Define your method here :)
+    let res = ""
+    for (let i = 0; i < this.length; i++) {
+        let c = this[i]
+        if (c.toUpperCase() == c) {
+            c = c.toLocaleLowerCase()
+        } else {
+            c = c.toUpperCase()
+        }
+        res += c
+    }
+    return res
+}
+
+// Usando array con join
+String.prototype.toAlternatingCase0 = function () {
+    // Define your method here :)
+    let res = []
+    for (let i = 0; i < this.length; i++) {
+        let c = this[i]
+        if (c.toUpperCase() == c) {
+            c = c.toLocaleLowerCase()
+        } else {
+            c = c.toUpperCase()
+        }
+        res.push(c)
+    }
+    return res.join('')
+}
+
+
+// String.prototype.toAlternatingCase = function () {
+//     // Define your method here :)
+// }
+
+
+// Pruebas 
+console.log("hello world".toAlternatingCase());
+console.log("HeLLo WoRLD".toAlternatingCase());
+console.log("12345".toAlternatingCase());
+console.log("Hello World".toAlternatingCase().toAlternatingCase())
 
 /*
 const { assert } = require('chai');
