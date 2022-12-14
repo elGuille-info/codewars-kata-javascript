1a58a95160db1b18856fd862a4e33eaebe6746b7
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-14 17:05:28 +0100

    invert

---
+++ codewars-kata-8 y 7 kyu/8-kyu-11-Invert values.js
@@ -0,0 +1,33 @@
+/*
+    # Invert values
+
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+
+LISTS, FUNDAMENTALS, ARRAYS
+*/
+
+function invert(array) {
+    return;
+}
+
+/*
+const Test = require('@codewars/test-compat');
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Invert array values", () => {
+    it("Basic Tests", () => {
+        assert.deepEqual(invert([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5]);
+        assert.deepEqual(invert([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5]);
+        assert.deepEqual(invert([]), []);
+        assert.deepEqual(invert([0]), [-0]);
+    });
+});
+*/
