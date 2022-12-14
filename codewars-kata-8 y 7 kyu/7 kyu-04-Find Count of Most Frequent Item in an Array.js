0ab26bb78d23c34472df03c6a60cc14414c767fd
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-14 22:36:57 +0100

    Find Count of Most Frequent Item in an Array

---
+++ codewars-kata-8 y 7 kyu/7 kyu-04-Find Count of Most Frequent Item in an Array.js
@@ -0,0 +1,28 @@
+/*
+    # Find Count of Most Frequent Item in an Array
+
+Complete the function to find the count of the most frequent item of an array. 
+    You can assume that input is an array of integers. For an empty array return 0
+
+Example
+input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+ouptut: 5 
+The most frequent number in the array is -1 and it occurs 5 times.
+
+DATA STRUCTURES, FUNDAMENTALS
+*/
+
+function mostFrequentItemCount(collection) {
+    // Do your magic. :)
+}
+
+/*
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+    it("test", () => {
+        Test.assertEquals(mostFrequentItemCount([3, -1, -1]), 2);
+        Test.assertEquals(mostFrequentItemCount([3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]), 5);
+    });
+});
+*/
