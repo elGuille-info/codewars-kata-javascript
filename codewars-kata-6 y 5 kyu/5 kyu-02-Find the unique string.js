4d84ae82645093827bc5bb30d682503a7101a6d6
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-12-15 18:12:53 +0100

    findUniq - soluciones

--- codewars-kata-6 y 5 kyu/5 kyu-02-Find the unique string.js
+++ codewars-kata-6 y 5 kyu/5 kyu-02-Find the unique string.js
@@ -24,9 +24,44 @@ Find The Unique (--> retirado)
 FUNDAMENTALS, ALGORITHMS, ARRAYS, STRINGS
 */
 
+/*
+    Soluciones
+*/
+
+//1- https://www.codewars.com/kata/reviews/585d8cdc211697ee7a0038e4/groups/586408758129984e9b000a48
+function findUniq_1(arr) {
+    let [a, b, c] = arr.slice(0, 3)
+
+    if (!similar(a, b) && !similar(a, c)) return a
+    for (d of arr) if (!similar(a, d)) return d
+}
+
+function similar(x, y) {
+    x = new Set(x.toLowerCase())
+    y = new Set(y.toLowerCase())
+
+    if (x.size !== y.size) return false
+    for (z of x) if (!y.has(z)) return false
+
+    return true
+}
+
+
+// Find the unique string ( 5 kyu )
+// 2018. 1. 19. 10:54
+// https://takeuu.tistory.com/239
+function findUniq(arr) {
+    let newArr = arr.map(a => { return [...new Set(a.toLowerCase())].sort().join('') });
+    for (let i = 0; i < newArr.length; i++) {
+        if (newArr.indexOf(newArr[i]) === newArr.lastIndexOf(newArr[i])) return arr[i]
+    }
+}
+
+// Presentada la de takeuu
+
 //String.prototype.replaceAllTxt = function replaceAll(search, replace) { return this.split(search).join(replace); }
 
-function findUniq(arr) {
+function findUniq0(arr) {
     // do magic
     console.log(arr)
 
@@ -35,7 +70,7 @@ function findUniq(arr) {
         //copia.push(arr[i].replaceAllTxt(' ', ''));
         copia.push(arr[i]);
     }
-    copia.sort( (a, b) => a.toLowerCase() === b.toLowerCase() ? 0 : a.toLowerCase() < b.toLowerCase() ? -1 : 1 );
+    copia.sort((a, b) => a.toLowerCase() === b.toLowerCase() ? 0 : a.toLowerCase() < b.toLowerCase() ? -1 : 1);
     let res = "";
     let una = copia[0];
     let esUna = false;
